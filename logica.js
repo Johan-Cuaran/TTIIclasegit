8fb012e09e590595fe8c6608764b09c521d79b51
Johan Cuaran <Cuaran.johan@correounivalle.edu.co>
2024-04-08 21:05:09 -0500

    segundos cambios de prueba

--- logica.js
+++ logica.js
@@ -3,5 +3,5 @@ console.log("esto lo que estoy escribiendo se vera en la consola de HTML")
 console.log("esta va ser la tercera version")
 console.log("este cambio ira hasta remoto");
 console.log("se agrega cambio en rama pruebas")
-
+console.log("Segundos cambios en rama pruebas")
 
