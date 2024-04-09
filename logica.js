18be2c64b33b54e621093f388ffec58bee09aa49
Johan Cuaran <Cuaran.johan@correounivalle.edu.co>
2024-04-08 20:56:11 -0500

    cambio en rama pruebas

--- logica.js
+++ logica.js
@@ -2,6 +2,6 @@ alert("Bienvenidos a Taller Tecnologico II")
 console.log("esto lo que estoy escribiendo se vera en la consola de HTML")
 console.log("esta va ser la tercera version")
 console.log("este cambio ira hasta remoto");
-
+console.log("se agrega cambio en rama pruebas")
 
 
